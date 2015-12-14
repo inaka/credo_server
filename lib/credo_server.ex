542c2665e407996cbfdb766a634cd3c3b5f576df
Iñaki Garay <igarai@gmail.com>
2015-12-14 11:43:45 -0300

    [#109536834] Initial project skeleton

---
+++ lib/credo_server.ex
@@ -0,0 +1,12 @@
+defmodule CredoServer do
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = []
+
+    opts = [strategy: :one_for_one, name: CredoServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end

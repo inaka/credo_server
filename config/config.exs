4e716921ba6785946968aa481ea032913d391a85
Alejandro Mataloni <amataloni@gmail.com>
2016-05-09 16:55:44 -0300

    [#111607294] refactor config files

--- config/config.exs
+++ config/config.exs
@@ -21,10 +21,27 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
+# Configure your database
+config :credo_server, CredoServer.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASS"),
+  database: System.get_env("DB_DATABASE"),
+  hostname: System.get_env("DB_HOST"),
+  pool_size: 10
+
+config :credo_server, :github_scope, "repo,user:email"
+config :credo_server, :github_client_id, System.get_env("GITHUB_CLIENT_ID")
+config :credo_server, :github_client_secret, System.get_env("GITHUB_CLIENT_SECRET")
+config :credo_server, :github_user, System.get_env("GITHUB_USER")
+config :credo_server, :github_password, System.get_env("GITHUB_PASSWORD")
+config :credo_server, :webhook_url, System.get_env("WEBHOOK_URL")
+config :credo_server, :secret_key_base, System.get_env("SECRET_KEY_BASE")
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
 #
-import_config "#{Mix.env}.exs"
+# import_config "#{Mix.env}.exs"
